68abe3205e8416650a02d3a4f05631019c97acbc
zhangguanzhang <zhangguanzhang@qq.com>
2019-11-28 21:14:32 +0800

    add the acfun

---
+++ acfun/README.md
@@ -0,0 +1,3 @@
+# for acfun post
+
+docker run -ti --rm -e username=xxx zhangguanzhang/acfun-dota https://www.youtube.com/watch?v=D6NQ1Z4iH5w
---
+++ acfun/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+set -e
+[[ "${DEBUG}" == "true" ]] && set -x
+down(){
+    youtube-dl --write-thumbnail \
+    --embed-sub \
+    --write-sub \
+    -f "bestvideo+bestaudio[ext=m4a]/bestvideo+bestaudio/best" --merge-output-format mp4 -o "a${1#*=}.%(ext)s" $1
+}
+url="$1"
+down "$url"
+ls -lh
+read -p 'input the title name: ' name
+[ -z "$password" ] && read -sp 'password: ' password
+acfun -u ${username} -p ${password} -n "${name}" --pic $(ls | grep .jpg) $(ls | grep -v jpg)
