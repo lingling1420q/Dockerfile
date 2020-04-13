531008a2b74f85bc522e59197393d075a0dac043
zhangguanzhang <zhangguanzhang@qq.com>
2020-04-13 13:24:41 +0800

    update the entrypoint.sh

--- acfun/entrypoint.sh
+++ acfun/entrypoint.sh
@@ -2,32 +2,57 @@
 set -e
 [[ "${DEBUG}" == "true" ]] && set -x
 cd /root/data
-youtube-dl -F $1
-read -t 7 -p 'select the quality: ' quality
-if [ -z "$quality" ];then
-    quality=bestvideo
-fi
+
+# $1 url [$2 quality]
 down(){
+    local quality='bestvideo'
+    local url=$1
+    if [ -n "$2" ];then
+        quality=$2
+    fi
     youtube-dl --write-thumbnail \
     --embed-sub \
     --write-sub \
-    -f "$quality+bestaudio[ext=m4a]/$quality+bestaudio/best" --merge-output-format mp4 -o "a${1#*=}.%(ext)s" $@
+    -f "$quality+bestaudio[ext=m4a]/$quality+bestaudio/best" --merge-output-format mp4 -o "a${1#*=}.%(ext)s" $url
 }
-if [ "${#@}" -ge 1 ];then
-  url="$@"
-  down "$url"
-fi
-if [ -n "$username" ];then
-    ls -lh
+
+
+
+upload(){
+    local url=$1
+    ls -alh
     if [ -n "$auto" ];then
         name=$(youtube-dl -f mp4 -o '%(id)s.%(ext)s' --print-json --no-warnings "$url" | jq -r .title)
-    else
-	read -p 'input the title name: ' name
+    fi
+    if [ -z "$name" ];then
+        read -p 'input the title name: ' name
     fi
     [ -z "$password" ] && {
         read -sp 'password: ' password
         echo
     }
+    if [ -n "$username" ];then
+        acfun -u ${username} -p ${password} -n "${name}" --pic $(ls | grep .jpg) *.mp4
+        rm -f *
+    fi
+}
 
-    acfun -u ${username} -p ${password} -n "${name}" --pic $(ls | grep .jpg) $(ls | grep -v jpg)
+
+if [ "$#" -gt 1 ];then
+    quality=bestvideo
+    for url in "$@";do
+        down $url
+        upload $url
+    done
+elif [ "$#" -eq 1 ];then #搬运单个大视频需要选择质量
+    youtube-dl -F $1
+    set +e
+    read -t 15 -p 'select the quality: ' quality
+    if [ -z "$quality" ];then
+        quality=bestvideo
+    fi
+    set -e
+    down $1 $quality
+else
+  sh
 fi
